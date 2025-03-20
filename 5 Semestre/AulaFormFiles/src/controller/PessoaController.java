7a1dab538083aaeb454c37392424ab09dc3e0872
felipecasali1 <felipecasali2604@gmail.com>
2025-03-20 02:14:47 -0400

    Finalizando o controller

--- 5 Semestre/AulaFormFiles/src/controller/PessoaController.java
+++ 5 Semestre/AulaFormFiles/src/controller/PessoaController.java
@@ -4,8 +4,9 @@ import models.Pessoa;
 import persistence.PessoaDAO;
 
 public class PessoaController {   
-    public static void salvar(String nome, String cpf, String cidade) {
+    public static boolean salvar(String nome, String cpf, String cidade) {
         Pessoa pessoa = new Pessoa(nome, cpf, cidade);
         PessoaDAO.salvar(pessoa);
+        return PessoaDAO.verificar(pessoa);
     }
 }
