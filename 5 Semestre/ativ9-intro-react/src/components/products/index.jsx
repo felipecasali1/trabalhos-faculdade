93b3b8cafdd92a472c2578ffe486bba9e67042d4
felipecasali1 <felipecasali2604@gmail.com>
2025-03-30 22:13:15 -0400

    Melhorias na estilização

--- 5 Semestre/ativ9-intro-react/src/components/products/index.jsx
+++ 5 Semestre/ativ9-intro-react/src/components/products/index.jsx
@@ -3,7 +3,8 @@ import { products } from "./products.js"
 import Cart from "../cart";
 import React, { useEffect } from "react";
 import { HiOutlineShoppingCart } from "react-icons/hi";
-
+import { HiOutlineChip } from "react-icons/hi";
+import { HiMenu } from "react-icons/hi";
 
 export default function Products() {
     const [cartItems, setCartItems] = React.useState([]);
@@ -24,15 +25,31 @@ export default function Products() {
     return (
         <div id="store-container">
             <div id="store-navbar">
-                <div id="store-name">Loja</div>
-            </div>
-            <div id="products-section">
-                <div className="product-area">
-
+                <div id="store-menu">
+                    <button><HiMenu size={45}/></button>
+                </div>
+                <div id="store-logo-name">
+                    <div id="store-logo">
+                        <HiOutlineChip size={45}/>
+                    </div>
+                    <div id="store-name">Loja</div>
+                </div>
+                <div id="cart-button">
+                    <button onClick={() => setShowCart(!showCart)}><HiOutlineShoppingCart size={30}/></button>
                 </div>
             </div>
-            <div id="cart-button">
-                <button onClick={() => setShowCart(!showCart)}><HiOutlineShoppingCart size={32}/></button>
+            <div id="products-section">
+                <div id="products">
+                    {
+                        products.map((item, index) => { return (
+                            <div className="product-area" key={index}>
+                                <div className="product-image">
+                                    <img src={item.image}/>
+                                </div>
+                            </div>
+                        )})
+                    }
+                </div>
             </div>
             <Cart cartItems={cartItems} setCartItems={setCartItems} showCart={showCart} setShowCart={setShowCart}/>
         </div>
